c1be276233201d8689bd2efeb8ca14222250e799
cyrus <cyruskarsan@gmail.com>
2020-10-18 11:16:13 -0700

    changed zoom

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 36.988407, lng: -122.058281 },
-    zoom: 8,
+    zoom: 12,
   });
 }
       
