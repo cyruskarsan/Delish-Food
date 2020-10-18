3176c1b96483df5bca81e432ea5febed0392f5c8
cyrus <cyruskarsan@gmail.com>
2020-10-18 11:08:15 -0700

    added simple gmaps webapp

---
+++ webapp/index.js
@@ -0,0 +1,23 @@
+// // Create the script tag, set the appropriate attributes
+// var script = document.createElement('script');
+// script.src = 'https://maps.googleapis.com/maps/api/js?key=YAIzaSyBliB0DVBbxnXcA-RBoYjpJxSaXYegoM9c&callback=initMap';
+// script.defer = true;
+
+// // Attach your callback function to the `window` object
+// window.initMap = function() {
+//   // JS API is loaded and available
+// };
+
+// // Append the 'script' element to 'head'
+// document.head.appendChild(script);
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+      
