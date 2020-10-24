f468bd9c7f030ee4e226d6e2bcb7c9e3b05eef34
William <williammai49@gmail.com>
2020-10-24 01:12:08 -0700

    added some comments

--- gmap.js
+++ gmap.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
-const places_api_key = process.env.API_KEY;
-// Create the script tag, set the appropriate attributes
+require('dotenv').config(); //makes dotenv an action (load anything in file called .env into a environment variable)
+const places_api_key = process.env.API_KEY; //sets the places_api_key to the API key
 
 //console.log(process.env); just a check to see if env variable was added
+
+// Create the script tag, set the appropriate attributes
 
 var script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBliB0DVBbxnXcA-RBoYjpJxSaXYegoM9c&callback=initMap';
