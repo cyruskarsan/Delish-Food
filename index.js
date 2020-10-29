015e9edf60ceea910c6afa101943ea0c24d52cea
cimontes <cimontes@ucsc.edu>
2020-10-28 17:39:54 -0700

    Geolocation: updated error handling

--- index.js
+++ index.js
@@ -47,6 +47,15 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed. Please enable your location."
+      : "Error: Your browser doesn't support geolocation."
+  );
+
   infoWindow.open(map);
 }
       
