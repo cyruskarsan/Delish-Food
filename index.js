726a44ab13166ade8b1c28150ce7b47cb8564cce
andgordo <andgordo@ucsc.edu>
2020-10-29 00:19:02 -0700

    Added UI-nav-menu features to core files, UI now in primary files

--- index.js
+++ index.js
@@ -17,6 +17,42 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 36.988407, lng: -122.058281 },
     zoom: 12,
+    disableDefaultUI: true,
   });
+
+  var mapStyle = [ // sets up getting rid of equator and international date line
+    {
+      featureType: "administrative",
+      elementType: "geometry",
+      stylers: [
+        { visibility: "off" } 
+      ]
+    }
+    ];
+    var styledMap = new google.maps.StyledMapType(mapStyle);
+    map.mapTypes.set('myCustomMap', styledMap);
+    map.setMapTypeId('myCustomMap');
 }
-      
+
+// DIV ELEMENT MOVEMENT SCRIPTS
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px
+  and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("box").style.marginLeft = "250px";
+  document.getElementById("map").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  console.log("opened nav");
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the 
+background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("box").style.marginLeft = "0";
+  document.getElementById("map").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+  console.log("closed nav");
+}
