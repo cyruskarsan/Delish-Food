a5637e8dca90a4d27994c8bf9f30b7193e0c7e0e
cyrus <cyruskarsan@gmail.com>
2020-10-31 13:33:34 -0700

    readd gmap.js

---
+++ gmap.js
@@ -0,0 +1,12 @@
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBliB0DVBbxnXcA-RBoYjpJxSaXYegoM9c&callback=initMap';
+script.defer = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
