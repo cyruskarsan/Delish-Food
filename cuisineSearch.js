79728f1e963863cb7303b3512cff1b32f74d2b41
andgordo <andgordo@ucsc.edu>
2020-11-03 19:44:34 -0800

    Added cuisineSearch.js cuisine search functions, moved index.js marker funcs, moved JS src index.html

---
+++ cuisineSearch.js
@@ -0,0 +1,79 @@
+//Run requests from cuisine type clicks
+function cuisineTypeSearch(request) {
+  //console.log(request);
+  //console.log(mapcenterpos);
+
+  //hit the gmaps places API and do a text search
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+
+  // show popup when click on marker
+  infowindow = new google.maps.InfoWindow();
+
+  //parse returned info from places
+  function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+              var place = results[i];
+              createMarker(place);
+
+          }
+      }
+  }
+
+  //create marker with icon and attributes
+  function createMarker(place) {
+      console.log(place)
+      const image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+      };
+      arguments
+
+      const marker = new google.maps.Marker({
+          map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location,
+      });
+
+      markers.push(marker);
+
+      google.maps.event.addListener(marker, "click", () => {
+          infowindow.setContent(place.name);
+          infowindow.open(map);
+      });
+  }
+}
+
+// Clear markers from previous search query
+function clearMarkers() {
+  for (let i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+  }
+}
+
+function cuisineTypeListener() {
+  /*onclick event listener for cuisine type options*/
+  let mysidenav = document.getElementById("mySidenav");
+  let cuisines = mysidenav.querySelectorAll('a.cuisine-type');
+  //console.log(cuisines);
+
+  for (let i = 0; i < cuisines.length; i++) {
+      let cuisine = cuisines[i]; // select individual cuisine type
+      cuisine.onclick = function() {
+          clearMarkers();
+          document.getElementById("demo").innerText = cuisine.innerHTML;
+          let request = {
+              location: new google.maps.LatLng(mapcenterpos[0], mapcenterpos[1], 14),
+              radius: "5",
+              type: "restaurant",
+              query: cuisine.innerText.toLowerCase(),
+          };
+          cuisineTypeSearch(request);
+      }
+  }
+}
--- index.js
+++ index.js
@@ -1,68 +1,21 @@
 var map;
 var service;
 var infowindow;
-
+var mapcenterpos;
+var markers = [];
 
 function initMap() {
   //SC coords
   var sc = new google.maps.LatLng(36.9723111, -122.0383785, 14);
 
-  // shwo popup when click on marker
+  // show popup when click on marker
   infowindow = new google.maps.InfoWindow();
 
   //create the map
   map = new google.maps.Map(document.getElementById("map"), {
     center: sc,
-    zoom: 14,
+    zoom: 13,
   });
-
-  //text request search
-  var request = {
-    location: sc,
-    radius: "5",
-    query: "Taqueria",
-  };
-
-  //hit the gmaps places API and do a text search
-  var service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-
-
-  //parse returned info from places
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        createMarker(results[i]);
-      }
-    }
-  }
-
-  //create marker with icon and attributes
-  function createMarker(place) {
-    console.log(place)
-    const image = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25),
-    };
-
-    const marker = new google.maps.Marker({
-      map,
-      icon: image,
-      title: place.name,
-      position: place.geometry.location,
-    });
-
-    google.maps.event.addListener(marker, "click", () => {
-      infowindow.setContent(place.name);
-      infowindow.open(map);
-    });
-  }
-
-
 
   var mapStyle = [ // sets up getting rid of equator and international date line
     {
@@ -79,35 +32,35 @@ function initMap() {
   map.setMapTypeId('myCustomMap');
 
   infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
+  /*const locationButton = document.createElement("button");
   locationButton.textContent = "Pan to Current Location";
   locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);*/
+  
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        //infoWindow.setPosition(pos);
+        //infoWindow.setContent("Location found.");
+        //infoWindow.open(map);
+        map.setCenter(pos);
+        mapcenterpos = pos;
+        cuisineTypeListener();
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
-
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -119,7 +72,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
   infoWindow.open(map);
 }
-
 
 // DIV ELEMENT MOVEMENT SCRIPTS
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px
