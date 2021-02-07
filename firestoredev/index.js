5e05e1864946dfece1c578d537a54b1d4e378076
andgordo <andgordo@ucsc.edu>
2021-02-06 20:25:16 -0800

    Firestore function test stuff

---
+++ firestoredev/index.js
@@ -0,0 +1,6 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
